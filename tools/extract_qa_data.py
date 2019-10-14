9c92d27f57be3bf612afd964e57cc84fd417076c
Minh Nguyen <minhnh.91@gmail.com>
2019-10-14 11:01:22 +0200

    add code to dump JSON data to file

--- tools/extract_qa_data.py
+++ tools/extract_qa_data.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import argparse
+import datetime
 import json
 import os
 from pymongo import MongoClient
@@ -9,7 +10,27 @@ DEFAULT_CONFIG_PATH = 'config/configs.json'
 DATABASE_NAME = 'digiklausur'
 
 
-def main(action, collection_name, database_name, config_file):
+def main(action, collection_name, database_name, config_file, output_file):
+    """
+    query MongoDB database and extract questions and answers
+
+    if 'action' == 'show', will show all available collections in database
+    if 'action' == 'extract', will dump QA data of the specified collection into a JSON file. Format of JSON file:
+        {
+            "<question_id>": {
+                "text": "<question_text>",
+                "answers": {
+                    "<answer_id>": {
+                        "text": "<answer_text>",
+                        "authors": { <username>: true },
+                        "ranked_by": { <username>: <ranking_score> }
+                    }
+                    ...
+                }
+            }
+            ...
+        }
+    """
     if not os.path.isfile(config_file):
         raise RuntimeError('config path is not a valid file: ' + config_file)
     with open(config_file) as in_file:
@@ -35,13 +56,27 @@ def main(action, collection_name, database_name, config_file):
         if not questions_doc['entries']:
             raise RuntimeError("no entry found in document 'questions'")
 
+        # get answers
+        question_data = {}
         for question_id, question_text in questions_doc['entries'].items():
-            print(question_id, question_text, '\n')
+            question_data[question_id] = {}
+            question_data[question_id]['text'] = question_text
+
             ans_doc = collection.find_one({'_id': 'answers_{}'.format(question_id)})
-            if not ans_doc:
-                print('no answer document for question: ' + question_id)
+            if not ans_doc or 'entries' not in ans_doc:
+                print('no answer for question: ' + question_id)
+                question_data[question_id]['answers'] = {}
                 continue
-            print(ans_doc)
+            question_data[question_id]['answers'] = ans_doc['entries']
+
+        # write to JSON file
+        if not output_file:
+            today_str = datetime.date.today().strftime('%Y-%m-%d')
+            output_file = '{}_{}_{}.json'.format(today_str, DATABASE_NAME, collection_name)
+
+        with open(output_file, 'w') as outfile:
+            json.dump(question_data, outfile, indent=2)
+
         return
 
     raise RuntimeError('unsupported script action: ' + action)
@@ -58,8 +93,10 @@ if __name__ == '__main__':
                         help='configuration file for the MongoDB server (default: {})'.format(DEFAULT_CONFIG_PATH))
     parser.add_argument('--database', '-d', default=DATABASE_NAME,
                         help='Name of database containing QA data (default: {})'.format(DATABASE_NAME))
+    parser.add_argument('--output-file', '-o', default=None, help='JSON file path to dump QA data')
     args = parser.parse_args()
     try:
-        main(args.action, args.collection, args.database, args.config_file)
+        main(args.action, args.collection, args.database, args.config_file, args.output_file)
     except Exception as e:
         print(e)
+        raise
